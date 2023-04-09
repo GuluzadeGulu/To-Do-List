29e5812453dba9b52cba8ecfa169723b6d1ec02a
Gulu Guluzade <mister.guluem25@gmail.com>
2023-04-09 16:46:47 +0400

    The To Do List was done.

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@ document.getElementById('button-add').addEventListener('click', addToDo);
 
 function addToDo () {
     const inputText = document.getElementById('add-todo');
-    if (!inputText){
+    if (!inputText.value){
         alert('Enter your plan.')
     }
     else{
         const todoList = document.querySelector('.todo-list');
         const newEl = document.createElement('li');
-        newEl.innerHTML = `<li class="todo">
-                                <div class="box">
-                                    <input type="checkbox">
-                                    <span>${inputText.value}</span>
-                                </div>
-                                <button class="delete-todo"><img src="images/icons8-cancel.svg" alt="Cancel"></button>
-                            </li>`;
+        newEl.innerHTML = `<div class="box">
+                                <input type="checkbox">
+                                <span>${inputText.value}</span>
+                            </div>
+                            <button class="delete-todo"><img src="images/icons8-cancel.svg" alt="Cancel"></button>`;
         todoList.appendChild(newEl);
         inputText.value = '';
+
+        document.querySelectorAll('.delete-todo').forEach((el) => {
+            el.addEventListener('click', () => {
+                el.parentNode.remove()
+            })
+        });
     }
 }
